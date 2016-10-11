b7f4c180ad9f48c21375ad96b23f9b5638d82669
Manpreet Singh <mannyy91s@gmail.com>
2016-10-11 14:08:32 -0400

    finished calc layout

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+function add(x,y){
+    var x=10;
+    var y=5;
+    var xy= x + y;
+    console.log(xy)
+}
+function subtract(x,y){
+    var x=10;
+    var y=5;
+    var xy= x - y;
+    console.log(xy)
+}
+function multiply(x,y){
+    var x=10;
+    var y=5;
+    var xy= x * y;
+    console.log(xy)
+}
+function divide(x,y){
+    var x=10;
+    var y=5;
+    var xy= x / y;
+    console.log(xy)
+}
